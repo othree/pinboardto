6a1352db45c53819defd8c13a27f567d3fbd7659
othree <othree@gmail.com>
2018-08-16 11:33:01 +0800

    Telegram should send post

--- core/recipes/telegram/__init__.py
+++ core/recipes/telegram/__init__.py
@@ -40,6 +40,6 @@ def to_telegram_bot(bookmarks, config):
                 "chat_id": chat_id,
                 "text": '%s %s' % (bookmark['extended'] or bookmark['description'], bookmark['href'])
             }
-            # r = requests.post('https://api.telegram.org/bot%s/sendMessage' % (config['api_token']), data=data)
+            r = requests.post('https://api.telegram.org/bot%s/sendMessage' % (config['api_token']), data=data)
             print('%s post to telegram %s' % (bookmark['href'], config['name']))
             time.sleep(.500)
